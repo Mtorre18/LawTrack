60bf5dd8a47c3b93362f71db908d5cbe8cc0dcf2
Moe <mtorre18@outlook.com>
2024-07-15 23:37:26 -0400

    Changed script file to insert records periodically, commented out scheduler

--- scripts/scheduler.js
+++ scripts/scheduler.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+//console.log(process.env); // Log all environment variables to check
 const mongoose = require('mongoose');
 const axios = require('axios');
 const Bill = require('../models/Bill'); 
-const dbURI=process.env.
+const dbURI=process.env.MONGO_URI;
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
@@ -9,16 +11,21 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 async function fetchAndUpdateBills() {
     try {
-        const lastUpdate = await Bill.findOne().sort({ updateDate: -1 }).select('updateDate');
-        const fromDate = lastUpdate ? new Date(lastUpdate.updateDate).toISOString() : new Date(0).toISOString();
-        const toDate = new Date().toISOString();
 
-        const response = await axios.get(`https://api.congress.gov/v3/bill?fromDateTime=${fromDate}&toDateTime=${toDate}&api_key=${process.env.c_API_KEY}`);
-        const bills = response.bills;
+    
+        //const lastUpdate = await Bill.findOne().sort({ updateDate: -1 }).select('updateDate');
+        //const fromDate = lastUpdate ? new Date(lastUpdate.updateDate).toISOString() : new Date(0).toISOString();
+        const toDate = new Date();
+        toDate.setDate(toDate.getDate() - 1);
+        const fromDate = new Date();
+        fromDate.setDate(fromDate.getDate() - 7);
 
-        for (let bill of bills) {
-            const subjectsResponse = await axios.get(`https://api.congress.gov/v3/bill/${bill.congress}/${bill.billType}/${bill.billNumber}/subjects?api_key=${process.env.c_API_KEY}`);
-            const subjects = subjectsResponse.subjects.legislativeSubjects.map(subject => subject.name);
+        const response = await axios.get(`https://api.congress.gov/v3/bill?fromDateTime=${formatDate(fromDate)}&toDateTime=${formatDate(toDate)}&api_key=${process.env.c_API_KEY}`);
+        const data = response.data;
+
+        for (let bill of data.bills) {
+            const subjectsResponse = await axios.get(`https://api.congress.gov/v3/bill/${bill.congress}/${bill.type.toLowerCase()}/${bill.number}/subjects?api_key=${process.env.c_API_KEY}`);
+            const subjects = subjectsResponse.data.subjects.legislativeSubjects.map(subject => subject.name);
 
             await Bill.updateOne(
                 { number: bill.number},
@@ -37,6 +44,9 @@ async function fetchAndUpdateBills() {
     } catch (error) {
         console.error('Error fetching or updating bills:', error);
     }
+}
+function formatDate(date) {
+    return date.toISOString().split('.')[0] + 'Z';
 }
 
 // Schedule the job to run daily at midnight
